10811b05d65286ca07b22b479c5a5e8a5d399a49
Prushak <makbuk@MacBook-Air-Makbuk.local>
2025-08-24 21:51:02 +0300

    refactoring

--- server.js
+++ server.js
@@ -1,81 +1,13 @@
-import express from "express";
+import express from 'express';
 import mysql from "mysql2/promise";
-import bodyParser from "body-parser";
 import cors from "cors";
 
 const app = express();
-const PORT = 3000;
-const MYSQL_URL =
-  "mysql://root:EcDYcKALedEALngttIPyFqsIYqFOzPig@caboose.proxy.rlwy.net:50875/railway";
+const PORT = process.env.PORT || 3000;
 
-console.log(process.env.prod_user);
+app.use(cors());
 
-app.use;
-cors({
-  origin: ["https://test-course-app.netlify.app", "http://localhost:3000"],
-  credentials: true,
-});
-// app.use(cors());
-app.use(bodyParser.json());
-
-// const dbConfig = {
-//   host: "caboose.proxy.rlwy.net",
-//   port: 50875,
-//   user: "root",
-//   password: "EcDYcKALedEALngttIPyFqsIYqFOzPig",
-//   database: "railway",
-// };
-
-const getDbConfig = () => {
-  if (MYSQL_URL) {
-    // Используем URL от Railway
-    return {
-      uri: MYSQL_URL,
-      waitForConnections: true,
-      connectionLimit: 10,
-      queueLimit: 0,
-    };
-  } else {
-    // Локальная разработка
-    return {
-      host: "caboose.proxy.rlwy.net",
-      port: 50875,
-      user: "root",
-      password: "EcDYcKALedEALngttIPyFqsIYqFOzPig",
-      database: "railway",
-      waitForConnections: true,
-      connectionLimit: 10,
-      queueLimit: 0,
-    };
-  }
-};
-
-const pool = mysql.createPool(getDbConfig());
-
-// export async function getCourses() {
-//   try {
-//     const [rows] = await pool.query("SELECT * FROM courses");
-//     console.log("Courses:", rows);
-//     return rows;
-//   } catch (err) {
-//     console.error("Ошибка при получении курсов:", err);
-//     throw err;
-//   }
-// }
-
-async function testConnection() {
-  try {
-    const connection = await pool.getConnection();
-    console.log("Successfully connected to MySQL database");
-    connection.release();
-  } catch (error) {
-    console.error("Database connection failed:", error);
-  }
-}
-
-testConnection();
-
-app.get("/courses", async (req, res) => {
+app.get("/courses", async (_, res) => {
   try {
     const [rows] = await pool.query("SELECT * FROM courses");
     res.json({ courses: rows, success: true });
@@ -85,16 +17,19 @@ app.get("/courses", async (req, res) => {
   }
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+const MYSQL_URL = "mysql://root:EcDYcKALedEALngttIPyFqsIYqFOzPig@caboose.proxy.rlwy.net:50875/railway";
+
+const getDbConfig = () => {
+  return {
+    uri: MYSQL_URL,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+  };
+};
+
+const pool = mysql.createPool(getDbConfig());
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server started on port ${PORT}`);
 });
-
-// process.on("SIGTERM", () => {
-//   console.log("Received SIGTERM, shutting down gracefully");
-//   process.exit(0);
-// });
-
-// process.on("SIGINT", () => {
-//   console.log("Received SIGINT, shutting down gracefully");
-//   process.exit(0);
-// });
